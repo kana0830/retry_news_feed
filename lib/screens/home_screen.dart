ce7f5bb3baaf40fcb6b488178de00510ce68c30d
kana <kf0830s@gmail.com>
2023-10-13 18:50:33 +0900

    BottomNavigationBarの追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:retry_news_feed/screens/pages/about_us_page.dart';
+import 'package:retry_news_feed/screens/pages/head_line_page.dart';
+import 'package:retry_news_feed/screens/pages/news_list_page.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,13 +9,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final _pages = [
+    HeadLinePage(),
+    NewsListPage(),
+    AboutUsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // TODO
-      body: Center(child: Container(
-        child: Text("米国が急伸"),
-      )),
+    return SafeArea(
+      child: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.highlight),
+                label: "トップニュース",
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.list),
+                label: "ニュース一覧",
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.info),
+                label: "このアプリについて",
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            }),
+      ),
     );
   }
 }
